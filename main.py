d10479ff16cfd35939ae469925cef54b82643ea0
Eduardo Imery <ejwimery@gmail.com>
2019-11-02 00:46:03 -0600

    Inserting one document to the DB

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ cluster = MongoClient("mongodb+srv://testpy:dY1HmsPgC74O4L1Q@gcpcluster-xvgjc.gc
 db = cluster["test"]
 collection = db["test"]
 
+
+# Insert one document to the DB
+post = {
+    "name":"kay",
+    "score":8
+    }
+
+collection.insert_one(post)
