e96b7b54c11bbbcaef112011db810401c4836fcb
Eduardo Imery <ejwimery@gmail.com>
2019-11-02 00:48:46 -0600

    Inserting many document to the DB

--- main.py
+++ main.py
@@ -7,9 +7,19 @@ collection = db["test"]
 
 
 # Insert one document to the DB
-post = {
-    "name":"kay",
-    "score":8
-    }
+#post = {
+#    "name":"kay",
+#    "score":8
+#    }
+#collection.insert_one(post)
 
-collection.insert_one(post)
+#Insert many documents in the DB
+post1 = {
+    "name":"Hector",
+    "score":23
+}
+post2 = {
+    "name":"Euclides",
+    "score":43
+}
+collection.insert_many([post1,post2])
