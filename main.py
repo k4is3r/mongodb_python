96111c54686fba1ce00db6013f5ae460c5114b46
Eduardo Imery <ejwimery@gmail.com>
2019-11-02 01:31:09 -0600

    Adding some new files wit the libraries

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pymongo
 from pymongo import MongoClient
 
-cluster = MongoClient("mongodb+srv://testpy:dY1HmsPgC74O4L1Q@gcpcluster-xvgjc.gcp.mongodb.net/test?retryWrites=true&w=majority")
+cluster = MongoClient("mongodb+srv://<user>:<pass>@gcpcluster-xvgjc.gcp.mongodb.net/test?retryWrites=true&w=majority")
 db = cluster["test"]
 collection = db["test"]
 
